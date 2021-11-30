1733f578757facd1f6b9900e1751f46b87d4ed41
Yoan @Ayolyan Bidet <yoan.bidet@gmail.com>
2021-11-30 17:59:32 +0100

    Whiteboard base

---
+++ WhiteBoard/public/sketch.js
@@ -0,0 +1,43 @@
+const PEN_SIZE = 10;
+
+var socket;
+var otherColors;
+
+function setup() {
+    createCanvas(600, 400);
+    background(51);
+
+    socket = io.connect('http://localhost:3000');
+    socket.on('mouse', otherDrawing)
+
+    otherColors = new Map();
+}
+
+function otherDrawing(data) {
+    var color = otherColors.get(data.id)
+    if (!otherColors.get(data.id)) {
+        otherColors.set(data.id, {
+            r: random(255),
+            g: random(255),
+            v: random(255)
+        })
+    }
+
+    noStroke();
+    fill(color.r, color.g, color.v);
+    ellipse(data.x, data.y, PEN_SIZE, PEN_SIZE);
+}
+
+function mouseDragged() {
+    console.log('Sending' + mouseX + ',' + mouseY);
+
+    var data = {
+        x: mouseX,
+        y: mouseY
+    }
+    socket.emit('mouse', data);
+
+    noStroke();
+    fill(255);
+    ellipse(mouseX, mouseY, PEN_SIZE, PEN_SIZE);
+}
